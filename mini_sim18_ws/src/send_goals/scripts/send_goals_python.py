746db6608bd0626a3671563d35c880e8b1aa8f9e
zhaoxiangli <zhaoxiangli@tencent.com>
2022-07-25 13:59:37 +0800

    update navigation params && send_goals py fole

--- mini_sim18_ws/src/send_goals/scripts/send_goals_python.py
+++ mini_sim18_ws/src/send_goals/scripts/send_goals_python.py
@@ -10,13 +10,15 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 def send_goals_python():
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
-    #定义四个发送目标点的对象
+
+    #定义 5 个发送目标点的对象
     goal0 = MoveBaseGoal()
-    goal1 = MoveBaseGoal() 
-    goal2 = MoveBaseGoal() 
+    goal1 = MoveBaseGoal()
+    goal2 = MoveBaseGoal()
     goal3 = MoveBaseGoal()
+    goal4 = MoveBaseGoal()
     
-    # 初始化四个目标点在 map 坐标系下的坐标,数据来源于《采集的目标点.docx》
+    # 初始化 5 个目标点在 map 坐标系下的坐标,数据来源于《采集的目标点.docx》
     goal0.target_pose.pose.position.x = 1.84500110149
     goal0.target_pose.pose.position.y = -0.883078575134
     goal0.target_pose.pose.orientation.z = -0.306595935327
@@ -36,45 +38,56 @@ def send_goals_python():
     goal3.target_pose.pose.position.y = 0.453477025032
     goal3.target_pose.pose.orientation.z = 0.946238058267
     goal3.target_pose.pose.orientation.w = -0.323471076121
-    
-    goal_lists=[goal0, goal1, goal2, goal3]       # 采用 python 中的列表方式，替代实现C/C++ 中的数组概念 
 
-    goal_number = 4     # total is 4 goals
+    goal4.target_pose.pose.position.x = 1.84500110149
+    goal4.target_pose.pose.position.y = -0.883078575134
+    goal4.target_pose.pose.orientation.z = -0.306595935327
+    goal4.target_pose.pose.orientation.w = 0.951839761956
+    
+    goal_lists=[goal0, goal1, goal2, goal3, goal4]       # 采用 python 中的列表方式，替代实现C/C++ 中的数组概念 
+
+    goal_number = 5     # total  goals:  5
     while(goal_number):      
-        if(4 - goal_number ==0):
-            goal_lists[4-goal_number].target_pose.header.frame_id = "map"
-            goal_lists[4-goal_number].target_pose.header.stamp = rospy.Time.now()
-            client.send_goal( goal_lists[4-goal_number])
-            str_log = "Send NO. %s Goal !!!" %str(4-goal_number)
+        if(5 - goal_number ==0):
+            goal_lists[5-goal_number].target_pose.header.frame_id = "map"
+            goal_lists[5-goal_number].target_pose.header.stamp = rospy.Time.now()
+            client.send_goal( goal_lists[5-goal_number])
+            str_log = "Send NO. %s Goal !!!" %str(5-goal_number)
             rospy.loginfo(str_log)
-        elif(4 - goal_number ==1):
-            goal_lists[4-goal_number].target_pose.header.frame_id = "map"
-            goal_lists[4-goal_number].target_pose.header.stamp = rospy.Time.now()
-            client.send_goal( goal_lists[4-goal_number])
-            str_log = "Send NO. %s Goal !!!" %str(4-goal_number)
+        elif(5 - goal_number ==1):
+            goal_lists[5-goal_number].target_pose.header.frame_id = "map"
+            goal_lists[5-goal_number].target_pose.header.stamp = rospy.Time.now()
+            client.send_goal( goal_lists[5-goal_number])
+            str_log = "Send NO. %s Goal !!!" %str(5-goal_number)
             rospy.loginfo(str_log)
-        elif(4 - goal_number ==2):
-            goal_lists[4-goal_number].target_pose.header.frame_id = "map"
-            goal_lists[4-goal_number].target_pose.header.stamp = rospy.Time.now()
-            client.send_goal( goal_lists[4-goal_number])
-            str_log = "Send NO. %s Goal !!!" %str(4-goal_number)
+        elif(5 - goal_number ==2):
+            goal_lists[5-goal_number].target_pose.header.frame_id = "map"
+            goal_lists[5-goal_number].target_pose.header.stamp = rospy.Time.now()
+            client.send_goal( goal_lists[5-goal_number])
+            str_log = "Send NO. %s Goal !!!" %str(5-goal_number)
             rospy.loginfo(str_log)
-        elif(4 - goal_number ==3):
-            goal_lists[4-goal_number].target_pose.header.frame_id = "map"
-            goal_lists[4-goal_number].target_pose.header.stamp = rospy.Time.now()
-            client.send_goal( goal_lists[4-goal_number])
-            str_log = "Send NO. %s Goal !!!" %str(4-goal_number)
+        elif(5 - goal_number ==3):
+            goal_lists[5-goal_number].target_pose.header.frame_id = "map"
+            goal_lists[5-goal_number].target_pose.header.stamp = rospy.Time.now()
+            client.send_goal( goal_lists[5-goal_number])
+            str_log = "Send NO. %s Goal !!!" %str(5-goal_number)
+            rospy.loginfo(str_log)
+        elif(5 - goal_number ==4):
+            goal_lists[5-goal_number].target_pose.header.frame_id = "map"
+            goal_lists[5-goal_number].target_pose.header.stamp = rospy.Time.now()
+            client.send_goal( goal_lists[5-goal_number])
+            str_log = "Send NO. %s Goal !!!" %str(5-goal_number)
             rospy.loginfo(str_log)
 
         wait = client.wait_for_result(rospy.Duration.from_sec(30.0))     # 发送完毕目标点之后，根据action 的机制，等待反馈执行的状态，等待时长是：30 s.
         if not wait:
-            str_log="The NO. %s Goal Planning Failed for some reasons" %str(4-goal_number)
+            str_log="The NO. %s Goal Planning Failed for some reasons && try again." %str(5-goal_number)
             rospy.loginfo(str_log)
-            goal_number = goal_number - 1        # 等待超时之后 准备发送下一个目标点，action server 会自动执行最新的请求.
             continue
         else:
-            str_log="The NO. %s Goal achieved success !!!" %str(4-goal_number)
+            str_log="The NO. %s Goal achieved success && wait for 5 s !!!" %str(5-goal_number)
             rospy.loginfo(str_log)
+            rospy.sleep(5.0)   # wait for 5 second.
             goal_number = goal_number - 1
     return "Mission Finished."
 
